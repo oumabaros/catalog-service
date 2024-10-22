0c64a12578251b3472994359e4cba7a96c5f4472
Barasa Ouma Peter <oumabaros@gmail.com>
2024-10-22 16:27:17 +0300

    First Commit

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,12 +1,10 @@
 package com.polarbookshop.catalogservice.domain;
 
 import java.time.Instant;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
-
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
