faaeffb9682306d703c5a9db0f235d6cb48e00d9
Barasa Ouma Peter <oumabaros@gmail.com>
2024-10-22 12:13:19 +0300

    First Commit

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -12,30 +12,40 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
 
-public record Book(
+public record Book (
 
-                @Id Long id,
-        
-                @NotBlank(message = "The book ISBN must be defined.") @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.") String isbn,
+        @Id
+        Long id,
 
-                @NotBlank(message = "The book title must be defined.") String title,
+        @NotBlank(message = "The book ISBN must be defined.")
+		@Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
+        String isbn,
 
-                @NotBlank(message = "The book author must be defined.") String author,
+        @NotBlank(message = "The book title must be defined.")
+        String title,
 
-                @NotNull(message = "The book price must be defined.") @Positive(message = "The book price must be greater than zero.") Double price,
+        @NotBlank(message = "The book author must be defined.")
+        String author,
 
-                @CreatedDate 
-                Instant createdDate,
+        @NotNull(message = "The book price must be defined.")
+        @Positive(message = "The book price must be greater than zero.")
+        Double price,
 
-                @LastModifiedDate 
-                Instant lastModifiedDate,
+        String publisher,
 
-                @Version int version
+        @CreatedDate
+        Instant createdDate,
 
-) {
+        @LastModifiedDate
+        Instant lastModifiedDate,
 
-        public static Book of(String isbn, String title, String author, Double price) {
-                return new Book(null, isbn, title, author, price, null, null, 0);
+        @Version
+        int version
+
+){
+
+        public static Book of(String isbn, String title, String author, Double price, String publisher) {
+                return new Book(null, isbn, title, author, price, publisher, null, null, 0);
         }
 
 }
--- src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -1,21 +1,24 @@
 package com.polarbookshop.catalogservice.web;
 
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.domain.BookService;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import com.polarbookshop.catalogservice.domain.BookNotFoundException;
-import com.polarbookshop.catalogservice.domain.BookService;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
 class BookControllerMvcTests {
-    
+
     @Autowired
     private MockMvc mockMvc;
+
     @MockBean
     private BookService bookService;
 
@@ -23,7 +26,9 @@ class BookControllerMvcTests {
     void whenGetBookNotExistingThenShouldReturn404() throws Exception {
         String isbn = "73737313940";
         given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
-        mockMvc.perform(get("/books/" + isbn)).andExpect(status().isNotFound());
-
+        mockMvc
+                .perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
     }
+
 }
